8e1518c3d45514eed9f02d55a2c75e4baecf2ca7
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 19:01:24 +0600

    for loop

--- index.php
+++ index.php
@@ -4,15 +4,7 @@
 $brands = ['Apple', 'Microsoft', 'huawei'];
 
 
-$i = 1;
-
-// while($i < 10){
-//     echo $i++;
-// }
-
-do{
-    echo $i++;
-    break;
-}while($i < 10);
-
+for($i = 1; $i <= 10; $i++){
+    echo $i;
+}
 
