88fcc933238d4cfc6cecdce3bd8d1b43f8f236ee
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 16:08:16 +0600

    assignment operators

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php 
 
-// Arithmetic Operators
+// Assignment Operators
 
 $num1 = 10;
 $num2 = 4;
 
-echo $num1 + $num2;
-echo $num1 - $num2;
-echo $num1 * $num2;
-echo $num1 / $num2;
-echo $num1 ** $num2;
-echo $num1 % $num2;
+$num1 += $num2;
+$num1 -= $num2;
+$num1 *= $num2;
+$num1 /= $num2;
+$num1 %= $num2;
+
+echo $num1;
