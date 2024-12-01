a1f5ed0b53555f1a1c0527401396d2bb2b15778e
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 15:40:30 +0600

    data types

--- index.php
+++ index.php
@@ -2,8 +2,30 @@
 
 $name = "Code with iSagar";
 
-$myFirstname = "iSagar";
+$sentence = "What's up iSagar"; // String
 
-$my_first_name = "iSagar";
+$age = 27; // Integer
 
-$MyFirstName = "iSagar";
+$ageMinus = -27; // Integer
+
+$price = 10.50; // Float
+
+$isTrue = true; // Boolean
+
+$isFalse = false; 
+
+$brands = array('Apple', 'Microsoft', 'Huawei', 10); // Array
+
+$clubs = [
+    'BAR' => 'Barcelona',
+    'RM' => 'Real Madrid'
+];
+
+$isEmpty = NULL; // NULL
+
+var_dump($clubs);
+print_r($price);
+echo "My name is iSagar";
+echo $ageMinus;
+print $ageMinus;
+print($age);
