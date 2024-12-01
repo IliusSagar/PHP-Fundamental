718a79bf24f4ef4ea39ec3e1a76013c202a97e2b
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 16:27:45 +0600

    logical operators

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php 
 
-// Comparison Operators
+// logical Operators
 
-$num1 = 10;
-$num2 = 4;
+$username = "isagar";
+$password = "1234";
 
-var_dump($num1 === $num2);
-var_dump($num1 !== $num2);
-var_dump($num1 >= $num2);
-var_dump($num1 <= $num2);
+if($username == "isagar" || $password == "1234"){
+    echo "Passed";
+}else{
+    echo "Failed";
+}
+
+// var_dump(
+//     $username == "isagar" && $password == "1234"
+// );
 
