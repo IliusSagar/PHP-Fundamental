2e6cc135015c1f9e35295034215f977125ef9ec4
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 15:42:41 +0600

    indirect reference

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php 
 
-$name = "Code with iSagar";
+$user = "Code with iSagar";
 
-$sentence = "What's up iSagar"; // String
+$$user = "iLius";
 
-$age = 27; // Integer
-
-$ageMinus = -27; // Integer
-
-$price = 10.50; // Float
-
-$isTrue = true; // Boolean
-
-$isFalse = false; 
-
-$brands = array('Apple', 'Microsoft', 'Huawei', 10); // Array
-
-$clubs = [
-    'BAR' => 'Barcelona',
-    'RM' => 'Real Madrid'
-];
-
-$isEmpty = NULL; // NULL
-
-var_dump($clubs);
-print_r($price);
-echo "My name is iSagar";
-echo $ageMinus;
-print $ageMinus;
-print($age);
+echo $user;
