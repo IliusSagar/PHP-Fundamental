dfae074b6f92f17159515fa512b2eb605e2fab97
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 18:19:56 +0600

    ternary operator

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
 
 $username = "codewithisagar";
 
-switch($username){
-    case strlen($username) < 10:
-        echo "Username has less than 10 characters";
-        break;
-    case strlen($username) < 20:
-        echo "Username has less than 20 characters";
-        break;
-    case strlen($username) < 30:
-        echo "Username has less than 30 characters";
-        break;
-    default:
-        echo "You are using too many characters";
-}
+// if($username) {
+//     echo "Username is {$username}";
+// }else{
+//     echo "Username has not been set";
+// }
+
+echo $username ? "Username is {$username}" : "Username has not been set";
+
 
